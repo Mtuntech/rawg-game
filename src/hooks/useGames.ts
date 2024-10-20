f0d9df5cc2a1aa9ca03392f1a73520981650c996
“Mtuntech” <mtun1997min@gmail.com>
2024-10-20 22:48:52 +0630

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/app-client";
 import { CanceledError } from "axios";
 
-export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-}
+export interface Platform { 
+    id: number;
+    name: string;
+    slug: string;
+  }
+  export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: { platform: Platform }[]
+  }
 
 interface FetchGameResponse {
   count: number;
