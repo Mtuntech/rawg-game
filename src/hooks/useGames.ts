4e07135dec4130eb673d7f699af26265995a1786
“Mtuntech” <mtun1997min@gmail.com>
2024-10-21 21:48:20 +0630

    Generic data fetching hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,49 +1,19 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/app-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-export interface Platform { 
-    id: number;
-    name: string;
-    slug: string;
-  }
-  export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-  }
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
 
+const useGames = () => useData<Game>('/games');
 
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<FetchGameResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
 export default useGames;
